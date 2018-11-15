5c3762eace6facdccbb53871f1590553c13d7e9e
saltyx <shiyan233@hotmail.com>
2018-11-15 21:46:56 +0800

    use asm to generate bytecode

--- src/main/java/me/learn/simplelang/main/Visitor.java
+++ src/main/java/me/learn/simplelang/main/Visitor.java
@@ -2,38 +2,47 @@ package me.learn.simplelang.main;
 
 import me.learn.simplelang.SimpleLangBaseVisitor;
 import me.learn.simplelang.SimpleLangParser;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public class Visitor extends SimpleLangBaseVisitor<Object> implements Opcodes {
 
-public class Visitor extends SimpleLangBaseVisitor<Object> {
+    static ClassWriter classWriter ;
+    static MethodVisitor methodVisitor;
+    static final String OBJECT_CLASS = "java/lang/Object";
+    static final String INIT = "<init>";
+    static {
+        classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        classWriter.visit(V1_8,
+                ACC_PUBLIC, "Test", null, OBJECT_CLASS, new String[]{});
 
-    static Map<String, Integer> methodPointer = new HashMap<>();
-    static List<Command> methodCommands = new ArrayList<>();
+        methodVisitor = classWriter.visitMethod(ACC_PUBLIC,
+                INIT, "()V", null, null);
 
-    // temporary memory
-    static Map<String, Object> tempMemory = new HashMap<>();
+        methodVisitor.visitVarInsn(ALOAD, 0);
+        methodVisitor.visitMethodInsn(INVOKESPECIAL, OBJECT_CLASS,
+                INIT, "()V", false);
+        methodVisitor.visitInsn(RETURN);
+        methodVisitor.visitMaxs(0, 0);
+        methodVisitor.visitEnd();
+    }
 
-    static Map<String, Object> memory = new HashMap<>();
+    @Override
+    public Object visitSimpleLang(SimpleLangParser.SimpleLangContext ctx) {
+        System.out.println("simpleLang");
+        return super.visitSimpleLang(ctx);
+    }
 
     @Override
     public Object visitReturnStat(SimpleLangParser.ReturnStatContext ctx) {
-        if (ctx.var() != null) {
-            String var = ctx.var().getText();
-            Object result =  memory.get(var) == null ?
-                    tempMemory.get(var) : memory.get(var);
-            // clear temporary memory
-            tempMemory.clear();
-            return result;
-        }
 
         return visit(ctx.expr());
     }
 
     @Override
     public Object visitFunctionCall(SimpleLangParser.FunctionCallContext ctx) {
+
         return super.visitFunctionCall(ctx);
     }
 
@@ -78,30 +87,4 @@ public class Visitor extends SimpleLangBaseVisitor<Object> {
         return super.visitNumber(ctx);
     }
 
-    private static class CommandInterpreter {
-        public static Object interpretMethod(String methodName) {
-            int idx = methodPointer.get(methodName);
-            methodCommands.get(idx);
-            return runAt(idx);
-            //TODO
-        }
-
-        private static Object runAt(int idx) {
-            int cIdx = idx;
-            Command command = null;
-            while ( (command = methodCommands.get(cIdx)) != null
-                    && command.op != Command.Op.RET) {
-
-            }
-            return null;
-        }
-
-        private static Object run(Command cmd) {
-            switch (cmd.op) {
-
-            }
-
-            return null;
-        }
-    }
 }
---
+++ src/test/java/TestASM.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.reflect.Field;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class TestASM extends ClassLoader implements Opcodes {
+    public static void main(String[] args) throws Throwable {
+
+        ClassWriter cw1 = new ClassWriter(ClassWriter.COMPUTE_FRAMES );
+
+        cw1.visit(V1_8,
+                ACC_PUBLIC,
+                "TestASM", null, "java/lang/Object", new String[]{});
+
+        MethodVisitor mw = cw1.visitMethod(ACC_PUBLIC,
+                "<init>", "()V", null,
+                null);
+
+        mw.visitVarInsn(ALOAD, 0);
+        mw.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+        mw.visitInsn(RETURN);
+        mw.visitMaxs(0, 0);
+        mw.visitEnd();
+
+        MethodVisitor mw1 = cw1.visitMethod(ACC_PUBLIC+ACC_STATIC, "main",
+                "([Ljava/lang/String;)V", null ,null);
+        mw1.visitLdcInsn("hi from asm");
+        mw1.visitVarInsn(ASTORE, 1);
+        mw1.visitFieldInsn(GETSTATIC, "java/lang/System",
+                "out", "Ljava/io/PrintStream;");
+        mw1.visitVarInsn(ALOAD, 1);
+        mw1.visitMethodInsn(INVOKEVIRTUAL,
+                "java/io/PrintStream",
+                "println",
+                "(Ljava/lang/String;)V", false);
+        mw1.visitFieldInsn(GETSTATIC, "java/lang/System",
+                "out", "Ljava/io/PrintStream;");
+        mw1.visitInsn(ICONST_0);
+        mw1.visitInsn(ICONST_3);
+        mw1.visitMethodInsn(INVOKESTATIC,
+                "TestASM",
+                "test",
+                "(II)I", false);
+        mw1.visitMethodInsn(INVOKEVIRTUAL,
+                "java/io/PrintStream",
+                "println",
+                "(I)V", false);
+        mw1.visitInsn(RETURN);
+        mw1.visitMaxs(0, 0);
+        mw1.visitEnd();
+
+        MethodVisitor instanceMethod = cw1.visitMethod(ACC_PUBLIC
+                        +ACC_STATIC,
+                "test","(II)I", null, null);
+        // 静态方法从0开始取 两个入参，a, b
+        Label label = new Label();
+        instanceMethod.visitVarInsn(ILOAD, 0); // a
+        instanceMethod.visitVarInsn(ILOAD, 1); // b
+        instanceMethod.visitJumpInsn(IF_ICMPLE, label); // a<=b 跳转
+        instanceMethod.visitVarInsn(ILOAD, 0); // A
+        instanceMethod.visitInsn(IRETURN);
+        instanceMethod.visitLabel(label);
+        instanceMethod.visitIincInsn(0, 1);    // ++a
+        instanceMethod.visitVarInsn(ILOAD, 0);
+        instanceMethod.visitVarInsn(ILOAD, 1); // b
+        instanceMethod.visitMethodInsn(INVOKESTATIC,
+                "TestASM", "test", "(II)I", false); // test(a, b)
+        instanceMethod.visitInsn(IRETURN); // return
+        instanceMethod.visitMaxs(0, 0);
+        instanceMethod.visitEnd();
+
+
+        byte[] data = cw1.toByteArray();
+        File file = new File("TestASM.class");
+        FileOutputStream fout = new FileOutputStream(file);
+        fout.write(data);
+        fout.close();
+
+        TestASM loader = new TestASM();
+        Class<?> c = loader.defineClass("TestASM", data, 0, data.length);
+        System.out.println(c.getName());
+        c.getMethods()[0].invoke(null, new Object[] { null });
+    }
+
+}
